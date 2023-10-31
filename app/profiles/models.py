9cf71b072714dd5c133d266a9ddb1d03ef25e47c
Robert Anderco <andercorobert@pc-131-125.customer.ask4.lan>
2023-10-31 09:21:43 +0000

    adding images to profiles and displaying profile on page if user is logged in

--- app/profiles/models.py
+++ app/profiles/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
+import uuid, os
 # Create your models here.
+
+def get_upload_path(instance, filename):
+    ext= filename.split('.')[-1]
+    filename = "{}.{}".format(uuid.uuid4(), ext)
+    return os.path.join('uploads/profile/img', filename)
+
 
 #one to one relationship - one user has one profile
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField()
-    profile_picture = models.ImageField(blank=True)
+    profile_picture = models.ImageField(upload_to=get_upload_path, default="uploads/profile/img/avatar.png", validators=[FileExtensionValidator(['png','jpg', 'jpeg'])])
     team = models.CharField(max_length=200)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
