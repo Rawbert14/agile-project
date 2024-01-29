a33fea8fde0e646640ca2d2c7f20416917fa48f3
Robert Anderco <andercorobert@Roberts-MacBook-Pro.local>
2024-01-29 18:53:53 +0000

    trying to fix

--- app/agile/settings.py
+++ app/agile/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -206,10 +205,3 @@ MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 MEDIA_ROOT = BASE_DIR /'media'
 
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-
-django_heroku.settings(locals())
