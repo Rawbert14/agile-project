12254c12325fde13ee59ecf66177dfee550be2f0
Robert Anderco <andercorobert@Roberts-MacBook-Pro.local>
2024-02-09 23:27:56 +0000

    final version

--- app/reports/forms.py
+++ app/reports/forms.py
@@ -41,7 +41,6 @@ class ProblemReportedForm(forms.ModelForm):
         #fields = '__all__'
         exclude = ('user', 'report', 'problem_id',)
         
-        
 class ReportResultForm(forms.Form):
     production_line = forms.ModelChoiceField(queryset=ProductionLine.objects.all())
     day = forms.CharField(widget=forms.DateTimeInput(attrs= {'class': 'datepicker'}))
